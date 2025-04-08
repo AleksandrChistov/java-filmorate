36a269ca3b25ed27e18e9c17b6847290209cc787
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-04-08 09:38:19 +0700

    create FilmService

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FilmService {
+    private final FilmStorage filmStorage;
+
+    public List<Film> getAll() {
+        return filmStorage.getAll();
+    }
+
+    public Film create(Film newFilm) {
+        return filmStorage.add(newFilm);
+    }
+
+    public Film update(Film newFilm) {
+        return filmStorage.update(newFilm);
+    }
+
+    public Film delete(int filmId) {
+        return filmStorage.delete(filmId);
+    }
+
+    public void addLike(int filmId, int userId) {
+        filmStorage.addLike(filmId, userId);
+    }
+
+    public void deleteLike(int filmId, int userId) {
+        filmStorage.deleteLike(filmId, userId);
+    }
+
+    public List<Film> getTop10Films() {
+        return filmStorage.getTop10Films();
+    }
+}
