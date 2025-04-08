ee3fca385a8bd2167b464b67ba51ddf3a8fe5e0e
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-04-08 12:59:39 +0700

    fix GlobalExceptionHandler

--- src/main/java/ru/yandex/practicum/filmorate/excepton/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/excepton/GlobalExceptionHandler.java
@@ -4,22 +4,26 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(Exception e) {
         return new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException e) {
         return new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({Throwable.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnexpectedException(Throwable e) {
         return new ErrorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
