6bcfcaaf7c491474acaef5a9159c5cab35bd3611
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-04-08 09:46:40 +0700

    create UserService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,62 +1,35 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.excepton.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.validation.Group;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-import static ru.yandex.practicum.filmorate.util.CommonUtil.getNextId;
 
 @RestController
 @RequestMapping(UserController.URL)
-@Slf4j
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     public static final String URL = "/users";
-    private final Map<Integer, User> users = new HashMap<>();
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.getAll();
     }
 
     @PostMapping
     public User create(@Validated(Group.OnCreate.class) @RequestBody User newUser) {
-        log.info("Добавление нового пользователя");
-        setLoginIfNameIsBlank(newUser);
-        newUser.setId(getNextId(users));
-        log.debug("Пользователю был присвоен id = {}", newUser.getId());
-        users.put(newUser.getId(), newUser);
-        log.info("Пользователь успешно добавлен, id = {}", newUser.getId());
-        return newUser;
+        return userService.create(newUser);
     }
 
     @PutMapping
     public User update(@Validated(Group.OnUpdate.class) @RequestBody User newUser) {
-        log.info("Обновление пользователя с id = {}", newUser.getId());
-        setLoginIfNameIsBlank(newUser);
-        if (users.containsKey(newUser.getId())) {
-            User oldUser = users.get(newUser.getId());
-            oldUser.setName(newUser.getName());
-            oldUser.setLogin(newUser.getLogin());
-            oldUser.setEmail(newUser.getEmail());
-            oldUser.setBirthday(newUser.getBirthday());
-            log.info("Пользователь успешно обновлен, id = {}", newUser.getId());
-            return oldUser;
-        }
-        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не был найден");
-    }
-
-    private static void setLoginIfNameIsBlank(User newUser) {
-        if (newUser.getName() == null || newUser.getName().isBlank()) {
-            newUser.setName(newUser.getLogin());
-            log.debug("Имя пользователя не заполнено, поэтому был присвоен логин вместо имени = {}", newUser.getName());
-        }
+        return userService.update(newUser);
     }
 
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+    private final UserStorage userStorage;
+
+    public List<User> getAll() {
+        return userStorage.getAll();
+    }
+
+    public User create(User newUser) {
+        log.info("Добавление нового пользователя");
+        setLoginIfNameIsBlank(newUser);
+        return userStorage.add(newUser);
+    }
+
+    public User update(User newUser) {
+        log.info("Обновление пользователя с id = {}", newUser.getId());
+        setLoginIfNameIsBlank(newUser);
+        return userStorage.update(newUser);
+    }
+
+    public User delete(int userId) {
+        return userStorage.delete(userId);
+    }
+
+    public void addFriend(int userId, int friendId) {
+        userStorage.addFriend(userId, friendId);
+    }
+
+    public void deleteFriend(int userId, int friendId) {
+        userStorage.deleteFriend(userId, friendId);
+    }
+
+    public List<User> getCommonFriends(int userId, int otherId) {
+        return userStorage.getCommonFriends(userId, otherId);
+    }
+
+    private static void setLoginIfNameIsBlank(User newUser) {
+        if (newUser.getName() == null || newUser.getName().isBlank()) {
+            newUser.setName(newUser.getLogin());
+            log.debug("Имя пользователя не заполнено, поэтому был присвоен логин вместо имени = {}", newUser.getName());
+        }
+    }
+}
