ea58daf552cbcc74cfebe786073bdeb7e667c03a
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-04-08 13:10:32 +0700

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> getAll();
+
     Film getById(int id);
+
     Film add(Film film);
+
     Film update(Film film);
+
     Film delete(int filmId);
+
     void addLike(int filmId, int userId);
+
     void deleteLike(int filmId, int userId);
+
     List<Film> getPopularFilmsByCount(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,12 +6,20 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> getAll();
+
     List<User> getFriends(int id);
+
     User getById(int id);
+
     User add(User user);
+
     User update(User user);
+
     User delete(int userId);
+
     void addFriend(int userId, int friendId);
+
     void deleteFriend(int userId, int friendId);
+
     List<User> getCommonFriends(int userId, int otherId);
 }
