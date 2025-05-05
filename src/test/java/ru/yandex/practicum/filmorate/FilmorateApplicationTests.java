27e2545a2423559c7645aa8ce2b0775e01c156c4
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-05-05 11:32:25 +0700

    fix FilmorateApplicationTests

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.dal.mapper.FilmRowMapper;
+import ru.yandex.practicum.filmorate.dal.mapper.FriendshipRowMapper;
+import ru.yandex.practicum.filmorate.dal.mapper.UserRowMapper;
+import ru.yandex.practicum.filmorate.dal.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.dal.storage.friendship.FriendshipDbStorage;
+import ru.yandex.practicum.filmorate.dal.storage.user.UserDbStorage;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({UserDbStorage.class, UserRowMapper.class, FriendshipDbStorage.class, FriendshipRowMapper.class, FilmDbStorage.class, FilmRowMapper.class})
 class FilmorateApplicationTests {
 
 	@Test
