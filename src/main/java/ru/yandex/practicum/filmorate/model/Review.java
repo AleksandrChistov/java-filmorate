1bf23733fa5502126e40251897d5c503f966b06f
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-05-23 10:18:47 +0700

    add reviews

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(of = "reviewId")
+public class Review {
+    private Long reviewId;
+    @NotBlank
+    private String content;
+    @NotNull
+    private Boolean isPositive;
+    @NotNull
+    private Long userId;
+    @NotNull
+    private Long filmId;
+    private int useful;
+}
